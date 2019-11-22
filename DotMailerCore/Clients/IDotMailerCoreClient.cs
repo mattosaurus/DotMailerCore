4e25b8e42e710ac9aa5a3ef02cbecf35b2ba107c
mattosaurus <mattosaurus@outlook.com>
2019-11-22 12:33:03 +0000

    Add rest of campaign methods to interface

--- DotMailerCore/Clients/IDotMailerCoreClient.cs
+++ DotMailerCore/Clients/IDotMailerCoreClient.cs
@@ -55,10 +55,54 @@ namespace DotMailerCore.Clients
 
         Task<List<Campaign>> GetCampaignsSentToSegmentAsync(int segmentId, int select, int skip);
 
+        Task<List<AddressBook>> GetCampaignAddressBooksAsync(int campaignId, int select, int skip);
+
+        Task<List<Campaign>> GetCampaignsWithActivitySinceDateAsync(int campaignId, DateTime dateTime, int select, int skip);
+
         Task<Campaign> GetCampaignAsync(int campaignId);
 
         Task<Campaign> GetCampaignWithDetailsAsync(int campaignId);
 
         Task<CampaignSummary> GetCampaignSummaryAsync(int campaignId);
+
+        Task<List<CampaignContactOpen>> GetCampaignOpensAsync(int campaignId, int select, int skip);
+
+        Task<List<CampaignContactOpen>> GetCampaignOpensAsync(int campaignId, int contactId, int select, int skip);
+
+        Task<List<CampaignContactOpen>> GetCampaignOpensAsync(int campaignId, DateTime dateTime, int select, int skip);
+
+        Task<List<CampaignContactSummary>> GetCampaignActivityAsync(int campaignId, int select, int skip);
+
+        Task<List<CampaignContactSummary>> GetCampaignActivityAsync(int campaignId, int contactId, int select, int skip);
+
+        Task<List<CampaignContactSummary>> GetCampaignActivityAsync(int campaignId, DateTime dateTime, int select, int skip);
+
+        Task<List<CampaignContactClick>> GetCampaignClicksAsync(int campaignId, int select, int skip);
+
+        Task<List<CampaignContactClick>> GetCampaignClicksAsync(int campaignId, int contactId, int select, int skip);
+
+        Task<List<CampaignContactClick>> GetCampaignClicksAsync(int campaignId, DateTime dateTime, int select, int skip);
+
+        Task<List<CampaignContactClick>> GetCampaignClicksWithLinkGroupsAsync(int campaignId, int select, int skip);
+
+        Task<List<CampaignContactClick>> GetCampaignClicksWithLinkGroupsAsync(int campaignId, int contactId, int select, int skip);
+
+        Task<List<CampaignContactClick>> GetCampaignClicksWithLinkGroupsAsync(int campaignId, DateTime dateTime, int select, int skip);
+
+        Task<List<CampaignContactPageView>> GetCampaignPageViewsAsync(int campaignId, int contactId, int select, int skip);
+
+        Task<List<CampaignContactPageView>> GetCampaignPageViewsSinceDateAsync(int campaignId, DateTime dateTime, int select, int skip);
+
+        Task<List<CampaignContactReply>> GetCampaignRepliesAsync(int campaignId, int contactId, int select, int skip);
+
+        Task<List<CampaignContactRoiDetail>> GetCampaignROIAsync(int campaignId, int contactId, int select, int skip);
+
+        Task<List<CampaignContactRoiDetail>> GetCampaignROIAsync(int campaignId, DateTime dateTime, int select, int skip);
+
+        Task<List<CampaignContactSocialBookmarkView>> GetCampaignSocialBookmarkViewsAsync(int campaignId, int select, int skip);
+
+        Task<List<CampaignContactSocialBookmarkView>> GetCampaignSocialBookmarkViewsAsync(int campaignId, int contactId, int select, int skip);
+
+        Task<List<Contact>> GetCampaignHardBouncesAsync(int campaignId, int select, int skip);
     }
 }
